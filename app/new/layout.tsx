4c8fb5a0bf2534718d339464e6b64aacbdf21922
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-07-16 17:41:20 +0530

    chore: changed the title on new group page

--- app/new/layout.tsx
+++ app/new/layout.tsx
@@ -1,11 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Finance Sync | New Group",
+};
+
 export default function NewGroupLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex items-center justify-center mx-4">
-      {children}
-    </div>
+    <div className="flex items-center justify-center mx-4">{children}</div>
   );
 }
